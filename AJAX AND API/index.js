81fb9c0f4d02d35d39fee07b718b34aa3c2d9bf5
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-04-24 06:32:07 +0100

    score keeper refractory done

--- AJAX AND API/index.js
+++ AJAX AND API/index.js
@@ -5,3 +5,30 @@ const square = (x) => multiply(x, x);
 const isRightTriangle = (a, b, c) => (
     square(a) + square(b) === square(c)
 )
+
+
+            
+            
+        
+    // const names = ['destiny', 'nonso', 'beast', 'evan', 'chairman', 'germstone', 'tolu'];
+    // let text = "";
+    // for (let i = 0; i < names.length; i++) {
+    //     text += names[i] + "<br>";
+    //     }{
+    //     console.log(text[i]);
+    // }
+
+
+//         var myStringArray =  ['destiny', 'nonso', 'beast', 'evan', 'chairman', 'germstone', 'tolu'];
+// var arrayLength = myStringArray.length;
+// for (var i = 0; i < arrayLength; i++) {
+//     console.log(myStringArray[i]);
+//     //Do something
+// }
+// const array =  ['destiny', 'nonso', 'beast', 'evan', 'chairman', 'germstone', 'tolu'];
+// array.forEach(function (item, index) {
+//   console.log(item, index);
+// });
+    
+
+  
