3434d2ade29e73dd20545fe95e2bb27eb5bd43d8
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-04-26 13:33:28 +0100

    promise intro

--- AJAX AND API/index.js
+++ AJAX AND API/index.js
@@ -1,34 +1,26 @@
-const multiply = (x, y) => x * y;
+// setTimeout(() => {
+//     document.body.style.backgroundColor = "yellow";
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = "orange";
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = "blue";
+//         }, 1000);
+//     }, 1000); 
+// }, 1000);
 
-const square = (x) => multiply(x, x);
-
-const isRightTriangle = (a, b, c) => (
-    square(a) + square(b) === square(c)
-)
-
-
-            
-            
-        
-    // const names = ['destiny', 'nonso', 'beast', 'evan', 'chairman', 'germstone', 'tolu'];
-    // let text = "";
-    // for (let i = 0; i < names.length; i++) {
-    //     text += names[i] + "<br>";
-    //     }{
-    //     console.log(text[i]);
-    // }
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+    }, delay);
+}
+delayedColorChange("yellow", 1000, () => {
+    console.log("yellow,");
+});
 
 
-//         var myStringArray =  ['destiny', 'nonso', 'beast', 'evan', 'chairman', 'germstone', 'tolu'];
-// var arrayLength = myStringArray.length;
-// for (var i = 0; i < arrayLength; i++) {
-//     console.log(myStringArray[i]);
-//     //Do something
-// }
-// const array =  ['destiny', 'nonso', 'beast', 'evan', 'chairman', 'germstone', 'tolu'];
-// array.forEach(function (item, index) {
-//   console.log(item, index);
-// });
-    
+
+
+
 
   
