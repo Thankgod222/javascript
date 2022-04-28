b71a4ad82db869a488507e1fab1ac3cdcf70963e
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-04-28 17:23:47 +0100

    enter

---
+++ OBJECT PROTOTYPE/index.js
@@ -0,0 +1,15 @@
+// String.prototype.yell = function() {
+//     return`OMG!!! ${this.toUpperCase()}!!!  AGGGSHHH!`;
+// }
+// Array.prototype.pop = function() {
+//     return'I WANT THAT ELEMENT, BUT I CAN\'T FIND IT!';
+// };
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Language: 
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const navColor = new Color('carrot', [230, 126, 34]);
+const logoColor = new Color('emerald', [0, 204, 113]);
+
