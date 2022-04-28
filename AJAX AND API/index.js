1ab1080eacdff3854f277252751c80a12c09bf2a
Thankgod222 <ogbonnathankgod285@gmail.com>
2022-04-28 08:38:07 +0100

    DONE AJAX/API

--- AJAX AND API/index.js
+++ AJAX AND API/index.js
@@ -1,22 +1,58 @@
-// setTimeout(() => {
-//     document.body.style.backgroundColor = "yellow";
-//     setTimeout(() => {
-//         document.body.style.backgroundColor = "orange";
-//         setTimeout(() => {
-//             document.body.style.backgroundColor = "blue";
-//         }, 1000);
-//     }, 1000); 
-// }, 1000);
+// const req = new XMLHttpRequest () ;
 
-const delayedColorChange = (newColor, delay, doNext) => {
-    setTimeout(() => {
-        document.body.style.backgroundColor = newColor;
-        doNext();
-    }, delay);
+// req.onload = function () {
+//     console.log('ALL DONE  WITH REQUEST!!!')
+//     const data = JSON.parse(this.responseText);
+    
+// }
+
+// req.onerror = function () {
+//     console.log('error!!!')
+//     console.log(this);
+// }
+// req.open('GET', 'https://api.tvmaze.com/search/shows?q=girls')
+// req.send();
+
+/*-------------------------------------------------------------------------*/
+/* FETCH */
+/*-------------------------------------------------------------------------*/
+
+// fetch('https://api.tvmaze.com/search/shows?q=girls')
+// .then(res => {
+//     console.log('RESPONSE, WAITING TO PARSE...', res)
+//     return res.json()
+// })
+// .then(data => {
+//     console.log('DATA PARSED...', data)
+// })
+// .catch(e => {
+//     console.log('OH NO! ERROR!', e)
+// })
+ 
+/*------------------------------------*/
+/* AXIOS -IMPORTANT */
+/*------------------------------------*/
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+    
+})
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img); 
+        }
+        
+    }
 }
-delayedColorChange("yellow", 1000, () => {
-    console.log("yellow,");
-});
+
 
 
 
--- PromisesIntro/creating promise/app.js
+++ PromisesIntro/creating promise/app.js
@@ -33,9 +33,9 @@ const delayedColorChange = (color, delay) => {
     })
 }
 delayedColorChange('red', 1000)
-.then(() => delayedColorChange('orange, 1000'))
-.then(() => delayedColorChange('yellow, 1000'))
-.then(() => delayedColorChange('green, 1000'))
-.then(() => delayedColorChange('blue, 1000'))
-.then(() => delayedColorChange('black, 1000'))
+.then(() =>delayedColorChange('orange', 1000))
+.then(() => delayedColorChange('yellow', 1000))
+.then(() => delayedColorChange('green', 1000))
+.then(() => delayedColorChange('blue', 1000))
+.then(() => delayedColorChange('black', 1000))
 
